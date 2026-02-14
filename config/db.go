16642be98afd48db0735629a4d6adfe7810d4fac
Sharath Kumar K <sharathkumar.k00@gmail.com>
2026-02-14 19:39:25 +0530

    Add 'godotenv' module to add '.env' file for storing secret

--- config/db.go
+++ config/db.go
@@ -1,15 +1,31 @@
 package config
 
 import (
+  "fmt"
+  "log"
+  "os"
+
+  "github.com/joho/godotenv"
   "gorm.io/driver/postgres"
   "gorm.io/gorm"
-  "log"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-  dsn := "host=localhost user=todo password=pass dbname=todo_db port=5432 sslmode=disable"
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+  dsn := fmt.Sprintf(
+    "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+    os.Getenv("DB_HOST"),
+    os.Getenv("DB_USER"),
+    os.Getenv("DB_PASSWORD"),
+    os.Getenv("DB_DBNAME"),
+    os.Getenv("DB_PORT"),
+  )
 
   db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
   if err != nil {
